fa48653170bb92e75414f69c859935b7ff17d4e6
orang3world <aaorange75@gmail.com>
2023-06-15 13:26:57 -0300

    script complete!

---
+++ base_text.js
@@ -0,0 +1,6 @@
+function base_text(name, surname, ultimo_mes, importe) {
+
+  var template_text = '***** ADMINISTRACION FUNDACION COMPROMISO *****\n\n Hola ! :\n  ' + name + ' ' + surname + '\n\n  En esta oportunidad,\n  te solicitamos la siguiente factura:\n\n  -----------------------------------------------\n  Mes Cerrado: ' + ultimo_mes + '\n  Por un importe de pesos: $ ' + importe + '\n  En concepto de: Honorarios Profesionales\n  -----------------------------------------------\n\n  Desde ya muchas gracias.\n  Seguimos en contacto.\n\n  Atentemente :\n  Marina Ehrman.\n  Directora de administración y finanzas\n  Fundación Compromiso\n'
+console.log(template_text)
+  return template_text;
+}
--- solicitudFactura.js
+++ solicitudFactura.js
@@ -20,7 +20,8 @@ Notas generales:
 // Variables para el uso de la fecha como nombre unico de las hojas creadas.
 
 var systemDate = Utilities.formatDate(new Date(), "GMT-3", "yyyyMMdd")
-var sendingDate = Utilities.formatDate(new Date(), "GMT-3", "yyyyMMdd \n HH:mm:ss")
+var sendingDate = Utilities.formatDate(new Date(), "GMT-3", "dd/MM/yyyy HH:mm:ss")
+var sendingDate2Lines = Utilities.formatDate(new Date(), "GMT-3", "dd/MM/yyyy \n HH:mm:ss")
 
 //=================================================================================
 function onOpen() {
@@ -29,11 +30,10 @@ function onOpen() {
   interfaz grafica de la planilla. 
   */
   //-------------------------------------------------------------------------------
-  const ui = SpreadsheetApp.getUi()
-
-  ui
+  SpreadsheetApp.getUi()
     .createMenu('EMAIL')
-    .addItem('INICIAR', 'emailPreview')
+    .addItem('INICIAR', 'starting')
+    .addItem('ENVIAR', 'enviar_mail')
     .addToUi();
 }
 //=================================================================================
@@ -143,170 +143,296 @@ function closedMonth() {
   return mesCerrado
 }
 //=================================================================================
-function reportBuilding() {
+function starting() {
   /*
- Esta funcion construye la hoja de registro nombrada con la fecha del sistema.
- */
+  Esta funcion lanza dos funciones para recoger los datos y enviarlos a las hojas 
+  'Informe' y la hoja de registro.
+  */
+  //-------------------------------------------------------------------------------
+  reportBuilding();
+  generar_informe();
+}
+//=================================================================================
+function reportBuilding() {
+  // Esta funcion construye la hoja de registro nombrada con la fecha del sistema.
   //-------------------------------------------------------------------------------
   var report = [] // inicializa la matriz para los datos de la hoja de registro.
   var sp = spAccess() // Acceso a la planilla
-  var ssValues = dataReading('Docentes', '') // matriz con datos de la hoja 'Docentes'
-  var ssHeaders = ssValues.shift() // separa los encabezados de la matriz anterior
-  var mesCerrado = closedMonth()
-
-  var lastColIndex = ssValues[0].length - 1 // indice de la ultima columna
-  var re = new RegExp(mesCerrado, "i") // patron para reconocer el mes cerrado
-  var monthInToHeader = ssHeaders[lastColIndex].search(re) // devuelve -1 si falso
+  var roles = ['Docentes', 'Coordinadores']
+  //var mesCerrado = closedMonth()
 
   //debugg console.log('ssHeaders : ' + ssHeaders)
   //debugg console.log('ssValues : ' + ssValues)
 
 
   //debugg console.log('mesCerrado : ' + mesCerrado)
-
-  var ssTemplateValues = dataReading('Plantillas', '') // matriz con datos de la hoja 'Docentes'
-  var textMessage = ssTemplateValues[1][1] // variable con la plantilla del mensaje de texto
-  var htmlMessage = ssTemplateValues[2][1] // variable con la plantilla del mensaje html
-
-  // Ingreso en la matriz de los encabezados
-  report.push(['NOMBRE', 'APELLIDO', 'E-MAIL', 'IMPORTE', 'MENSAJE-TEXTO', 'MENSAJE-HTML'])
-
-  // DATA ITERATION
-  for (let i = 0; i < ssValues.length; i++) {
-
-    //debugg console.log('ssValues[0].length : ' + ssValues[0].length + ' , value of i = ' + i)
-
-
-    var firstName = ssValues[i][0] // variable con los nombres de la persona
-    var lastName = ssValues[i][1] // variable con apellido de la persona
-    var email = ssValues[i][2] // variable con el email de la persona
-    var amount = ssValues[i][lastColIndex] // variable con el importe (ultima columna)
-    var giveName = firstName.replace(/(^.*) (.*)$/, "$1") // variable con el primero de los nombres
-
-    //debugg console.log('coincidencia re del mes cerrado en el encabezado de importes: '+monthInToHeader)
-
-    var ccemail = '' // variable con emails para copia carbon (cc)
-    var new_subject = '' // variable con el asunto del email
-    var body = textMessage
-    var htmlBody = htmlMessage
-
-    /* Busqueda de caracteres dentro de la plantilla de mensaje de texto
-    y reemplazo por el valor de la variable de igual nombre
-    */
-    var body = body.replace('{{lastName}}', lastName.toUpperCase())
-      .replace('{{firstName}}', firstName.toUpperCase())
-      .replace('{{giveName}}', giveName)
-      .replace('{{amount}}', new Intl.NumberFormat('es-AR',
-        {
-          style: 'currency',
-          currency: 'ARS',
-          maximumFractionDigits: 0
-        })
-        .format(amount))
-      .replace('{{mesCerrado}}', mesCerrado)
-
-    /* Busqueda de caracteres dentro de la plantilla de mensaje html
-  y reemplazo por el valor de la variable de igual nombre
+  /*
+    var ssTemplateValues = dataReading('Plantillas', '') // matriz con datos de la hoja 'Docentes'
+    var textMessage = ssTemplateValues[1][1] // variable con la plantilla del mensaje de texto
+    var htmlMessage = ssTemplateValues[2][1] // variable con la plantilla del mensaje html
   */
-    var htmlBody = htmlBody.replace('{{lastName}}', lastName.toUpperCase())
-      .replace('{{firstName}}', firstName.toUpperCase())
-      .replace('{{giveName}}', giveName)
-      .replace('{{amount}}', new Intl.NumberFormat('es-AR',
+  // Ingreso en la matriz de los encabezados
+  report.push(['NOMBRE', 'APELLIDO', 'E-MAIL', 'IMPORTE', 'MENSAJE-TEXTO', 'MENSAJE-HTML', 'ROL'])
+
+  for (let j = 0; j < roles.length; j++) { // Iteracion por las hojas con datos (en roles)
+
+    var ssValues = dataReading(roles[j], '') // matriz con datos de la hoja dentro de la matriz roles.
+    var ssHeaders = ssValues.shift() // separa los encabezados de la matriz anterior
+    var lastColIndex = ssValues[0].length - 1 // indice de la ultima columna
+    /*
+    var re = new RegExp(mesCerrado, "i") // patron para reconocer el mes cerrado
+    var monthInToHeader = ssHeaders[lastColIndex].search(re) // devuelve -1 si falso
+*/
+    // DATA ITERATION
+    for (let i = 0; i < ssValues.length; i++) {
+
+      //debugg console.log('ssValues[0].length : ' + ssValues[0].length + ' , value of i = ' + i)
+
+      var rol = roles[j]
+      var name = ssValues[i][0] // variable con los nombres de la persona
+      var surname = ssValues[i][1] // variable con apellido de la persona
+      var email = ssValues[i][2] // variable con el email de la persona
+      var importe = ssValues[i][lastColIndex].toString() // variable con el importe (ultima columna)
+      var importe = new Intl.NumberFormat('es-AR',
         {
           style: 'currency',
           currency: 'ARS',
           maximumFractionDigits: 0
         })
-        .format(amount))
+        .format(importe)
+      if (importe.charAt(0) == "$") {
+        // si el caracter con el indice 0 de la variable importe es $ se le asigana a la variable importe el contenido del string contando desde el indice 1
+        importe = importe.slice(1);
+      }
+      //var giveName = name.replace(/(^.*) (.*)$/, "$1") // variable con el primero de los nombres
+      var ultimo_mes = ssHeaders[lastColIndex].charAt(0).toUpperCase() + ssHeaders[lastColIndex].slice(1);
+
+      //debugg console.log('coincidencia re del mes cerrado en el encabezado de importes: '+monthInToHeader)
+      /*
+            var ccemail = '' // variable con emails para copia carbon (cc)
+            var new_subject = '' // variable con el asunto del email
+            var body = textMessage
+            var htmlBody = htmlMessage
+      */
+      /* Busqueda de caracteres dentro de la plantilla de mensaje de texto
+      y reemplazo por el valor de la variable de igual nombre
+      */
+      var codigo = base_html(name, surname, ultimo_mes, importe);
+      var body = base_text(name, surname, ultimo_mes, importe);
+
+      /*var body = body.replace('{{surname}}', surname.toUpperCase())
+      .replace('{{name}}', name.toUpperCase())
+      .replace('{{giveName}}', giveName)
+      .replace('{{importe}}', new Intl.NumberFormat('es-AR',
+        {
+          style: 'currency',
+          currency: 'ARS',
+          maximumFractionDigits: 0
+        })
+        .format(importe))
       .replace('{{mesCerrado}}', mesCerrado)
+*/
+      /* Busqueda de caracteres dentro de la plantilla de mensaje html
+      y reemplazo por el valor de la variable de igual nombre
+      
+      var htmlBody = htmlBody.replace('{{surname}}', surname.toUpperCase())
+        .replace('{{name}}', name.toUpperCase())
+        .replace('{{giveName}}', giveName)
+        .replace('{{importe}}', new Intl.NumberFormat('es-AR',
+          {
+            style: 'currency',
+            currency: 'ARS',
+            maximumFractionDigits: 0
+          })
+          .format(importe))
+        .replace('{{mesCerrado}}', mesCerrado)
+*/
+      //debugg console.log(body)
+      console.log(body)
+      //var emailVerification, sending, error
+      // TEXT MESSAGE
+      // armado de la fila de datos con las variables, para la matriz 'report'
+      var reportRow = [name, surname, email, importe, body, codigo, rol]
+      report.push(reportRow)// Carga de la fila a la matriz.
 
-    //debugg console.log(body)
-    console.log(body)
-    //var emailVerification, sending, error
-    // TEXT MESSAGE
-
-    var reportRow = [firstName, lastName, email, amount, body, htmlBody]
-    report.push(reportRow)
-
+    }
   }
-  // IF NOT EXIST CREATE SSREPORT
+  // Si la hoja de registro no existe, la crea en el indice numero 5, como oculta.
   if (!ssAccess(systemDate, '')) {
     sp.insertSheet(systemDate, 5).hideSheet()
   } else {
-    // DATA DELETE SSREPORT
-    ssAccess(systemDate, '').clearContents
+    // Si la hoja de registro ya existe, la limpia de datos viejos.
+    ssAccess(systemDate, '').clearContents()
   }
-  // DATA INPUT INTO SSREPORT
+  // carga los datos a la hoja de registro, con la matriz 'report' ya completa
   ssAccess(systemDate, '').getRange(1, 1, report.length, report[0].length).setValues(report)
 }
 
 //=================================================================================
 function emailPreview() {
-  /*
- Esta funcion permite una VISTA PREVIA de la version html del email.
- */
+  //Esta funcion permite una VISTA PREVIA de la version html del email.
   //-------------------------------------------------------------------------------
+
   try {
-    var ss = ssAccess(systemDate, '')
-
-
-    //ss.setActiveSelection().activateAsCurrentCell()
-    var rowSelection = ss.getSelection().getCurrentCell().getRow()
-    console.log('Current Cell: ' + rowSelection);
-
-    var htmlList = ss.getRange(1, 6, ss.getLastRow() - 1, 1).getValues()
-
-    console.log('htmlList : ' + htmlList[rowSelection])
-
-    //var html = HtmlService.createHtmlOutputFromFile('Template').setTitle('Email preview');
-
-    var html = HtmlService.createHtmlOutput("'" + htmlList[rowSelection - 1] + "'").setTitle('E-MAIL : VISTA PREVIA')
-    //var html = HtmlService.createHtmlOutput(dLoad())
-    //SpreadsheetApp.getUi().showModelessDialog(html, 'Email - Preview')
-    SpreadsheetApp.getUi().showSidebar(html)
+    if (!ssAccess(systemDate, '')) {
+      message('Estos datos ya fueron enviados\nVista Previa: no disponible')
+    } else {
+      var ss = ssAccess(systemDate, '')
+      var rowSelection = ss.getSelection().getCurrentCell().getRow()
+      //console.log('Current Cell: ' + rowSelection);
+      var htmlList = ss.getRange(1, 6, ss.getLastRow() - 1, 1).getValues()
+      //console.log('htmlList : ' + htmlList[rowSelection])
+      var html = HtmlService.createHtmlOutput("'" + htmlList[rowSelection - 1] + "'")
+        .setTitle('E-MAIL : VISTA PREVIA')
+      SpreadsheetApp.getUi().showSidebar(html)
+    }
   }
   catch (e) {
     messageDebugging(e)
   }
-  //SpreadsheetApp.getUi().showSidebar(html);
-  //SpreadsheetApp.getUi().showModalDialog(html, 'Email - Preview')
-
-}
-//=================================================================================
-function listend() {
-  /*
- Esta funcion realiza el envio de los emails.
- */
-  //-------------------------------------------------------------------------------
-  //var body = ssTemplateValues[1][1].replace(/\\n/g, "\n")
-  /*
-    Object.assign(list, {
-      [email]:
-      {
-        'to': email,
-        'cc': ccemail,
-        'subject': new_subject,
-        'body': body,
-        'htmlBody': htmlBody
-      }
-    })
-    */
-  // TIMESTAMP IN TAB OF SSREPORT
 }
 /*
-//=================================================================================
-function emailValidation(email) {
-  //-------------------------------------------------------------------------------
-  try {
-    SpreadsheetApp.getActive().addViewer(email)
-    //Exception: Invalid email: aaorange76@gmail.com
-  }
-  catch (e) {
-    console.log('Error identification: ' + e + ' note: ' + e.getStatusCode())
-  }
-  SpreadsheetApp.getActive().removeViewer('aaorange75@gmail.com')
-
-}
+AGREGO FUNCIONES "ENVIAR_MAIL" Y "GENERAR_INFORME".  
+ADEMAS, CREE UN BASE_HTML.GS PARA TRATAR EL CONTENIDO HTML 
+QUE SE ENVIA POR MAIL COMO UNA FUNCION 
+SIN MOLESTAR EN EL CODIGO PRINCIPAL
 */
 
+// 
+//=================================================================================
+function enviar_mail() {
+  /*
+  la funcion "enviar_mail" primero genera la planilla "log" con todos los datos
+   y luego envia cada uno de los mails
+   es importante que esta funcion se ejecute una vez este generado el informe y
+    chequeado que el mes sea el que corresponde
+   */
+  //-------------------------------------------------------------------------------
+  var ui = SpreadsheetApp.getUi()
+
+  var response = SpreadsheetApp.getUi()
+    .alert('ATENCION !\n\nEsta a punto de enviar : \n\n   ' + [dataReading(systemDate, '').length - 1] + ' emails.\nEsta acción no puede ser deshecha.\n\n¿ Desea continuar ?', ui.ButtonSet.YES_NO);
+
+  if (response == ui.Button.NO) {
+    SpreadsheetApp.getUi().alert('El envio de E-mails se ha\nCANCELADO');
+
+    return;
+
+  } else if (response == ui.Button.YES) {
+
+    SpreadsheetApp.getUi().alert('El envio de E-mails ha\nCOMENZADO')
+
+    var ssId = SpreadsheetApp.create('ssTest', 1, 1).getId()
+    var ss = SpreadsheetApp.openById(ssId)
+
+    var report = [];
+    var envios = [];
+    envios.push(['Envio\nÚltima Actualización:\n' + sendingDate])
+    var hoja = ["Docentes", "Coordinadores"];
+    report.push(['NOMBRE', 'APELLIDO', 'E-MAIL', "IMPORTE", 'ENVIO', 'MENSAJE-HTML']);
+
+    for (var x = 0; x < hoja.length; x++) { // Iteracion por las hojas de datos 
+      var data = dataReading(hoja[x], "");
+      var ssHeaders = data.shift();
+      var lastColIndex = data[0].length - 1;
+      var ultimo_mes = ssHeaders[lastColIndex].charAt(0).toUpperCase() + ssHeaders[lastColIndex].slice(1);
+
+      for (var i = 0; i < data.length; i++) { // Iteracion por los datos de cada hoja.
+        var row = data[i];
+        var name = row[0];
+        var surname = row[1];
+        var email = row[2];
+        var importe = row[lastColIndex].toString(); // pasamos al tipo de dato string para manejar mejor la informacion
+        var importe = new Intl.NumberFormat('es-AR',
+          {
+            style: 'currency',
+            currency: 'ARS',
+            maximumFractionDigits: 0
+          })
+          .format(importe)
+        if (importe.charAt(0) == "$") {
+          // si el caracter con el indice 0 de la variable importe es $ se le asigana a la variable importe el contenido del string contando desde el indice 1
+          importe = importe.slice(1);
+        }
+
+        var codigo = base_html(name, surname, ultimo_mes, importe);
+        var body = base_text(name, surname, ultimo_mes, importe);
+
+        if (importe.charAt(0) !== "0") {
+          // si el caracter ocn el indice 0 de la variable importe es distinto a 0 enviamos el mail
+          var subject = "Facturacion de Honorarios " + ultimo_mes + ' - ' + name + ' ' + surname;
+
+          try {
+            ss.addViewer(email)
+            MailApp.sendEmail(email, subject, body, { htmlBody: codigo });
+            var envio = 'Exitoso'
+          }
+          catch (e) {
+            var envio = e
+          }
+
+          report.push([name, surname, email, importe, envio, codigo]);
+          envios.push([envio])
+
+        }
+      }
+    }
+
+    DriveApp.getFileById(ssId).setTrashed(true)
+
+    if (!ssAccess(systemDate, '')) {
+      // en caso de que la planilla log no exista la crea
+      spAccess().insertSheet(systemDate).hideSheet();
+    }
+    // borramos el contenido de toda la planilla log correspondiente a la fecha actual y insertamos los datos
+    ssAccess(systemDate, '').clearContents();
+    ssAccess(systemDate, '').getRange(1, 1, report.length, report[0].length).setValues(report);
+    ssAccess(systemDate, '').setName(sendingDate2Lines)
+    ssAccess('Informe', '').getRange(1, 7, envios.length, 1).setValues(envios);
+  }
+  SpreadsheetApp.getUi().alert('El envio de E-mails ha\nCONCLUIDO')
+}
+
+//=================================================================================
+function generar_informe() {
+  /*
+ la funcion "generar_informe" busca generar una hoja dentro de la planilla para que puedan tener informacion de los docentes y coordinadores mas al alcance
+ */
+  //-------------------------------------------------------------------------------
+
+  var report = [];
+  var hoja = ["Docentes", "Coordinadores"];
+  //report.push(['Nombre', 'Apellido', 'Email', "Rol", "Mes", "Honorarios", "Envio", "Última Actualización", sendingDate])
+  report.push(['Nombre', 'Apellido', "Rol", "Mes", "Honorarios", '  Email  ', "Envio\nÚltima Actualización:\n" + sendingDate])
+  for (var x = 0; x < hoja.length; x++) {
+    var data = dataReading(hoja[x], "");
+    var ssHeaders = data.shift();
+    var lastColIndex = data[0].length - 1;
+    var ultimo_mes = ssHeaders[lastColIndex].charAt(0).toUpperCase() + ssHeaders[lastColIndex].slice(1);
+    for (var i = 0; i < data.length; i++) {
+      var row = data[i];
+      var name = row[0];
+      var surname = row[1];
+      var email = row[2];
+      var importe = row[lastColIndex].toString();
+      var importe = new Intl.NumberFormat('es-AR',
+        {
+          style: 'currency',
+          currency: 'ARS',
+          maximumFractionDigits: 0
+        })
+        .format(importe)
+      if (importe.charAt(0) == "$") {
+        importe = importe.slice(1);
+      }
+      //report.push([name, surname, email, hoja[x], ultimo_mes, importe, "", "", ""]);
+      report.push([name, surname, hoja[x], ultimo_mes, importe, email, ""]);
+    }
+  }
+  if (!ssAccess("Informe", "")) {
+    spAccess().insertSheet("Informe");
+  }
+  ssAccess("Informe", "").clearContents();
+  ssAccess("Informe", "").getRange(1, 1, report.length, report[0].length).setValues(report);
+}
 
