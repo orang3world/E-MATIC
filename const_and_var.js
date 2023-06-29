7c76dd668c5c5566691f301922767855e473c811
orang3world <aaorange75@gmail.com>
2023-06-28 22:56:34 -0300

    Actualizacion del estado final del script antes de la entrega y ya probado.

---
+++ const_and_var.js
@@ -0,0 +1,9 @@
+var roles = ['Docentes', 'Coordinadores']
+var meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Setiembre', 'Octubre', 'Noviembre', 'Diciembre']
+var hoja = roles;
+
+var theme = {
+    'Docentes': 'f1e3e3',
+    'Coordinadores': 'eef8de',
+    'Becados': 'e0f3fa'
+}
